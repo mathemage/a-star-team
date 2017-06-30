a8857a10e45ab8d0ad9e3906400ad00f7f97f110
Petr Marek <ermrk.marek@gmail.com>
2017-06-30 20:15:16 +0200

    Get positives and negatives

--- send_invoice.py
+++ send_invoice.py
@@ -11,4 +11,23 @@ def send_invoice(url):
     print(r.data)
 
 
-send_invoice("https://www.cca.edu/sites/default/files/pdf/08/word-to-pdf.pdf")
+def get_positives():
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'http://34.251.41.213:5000/url/positives',
+                     headers={'Authorization': 'secret_key romanjevsechno'})
+    to_return = json.loads(r.data.decode('utf-8'))
+    return to_return
+
+
+def get_negatives():
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'http://34.251.41.213:5000/url/negatives',
+                     headers={'Authorization': 'secret_key romanjevsechno'})
+    to_return = json.loads(r.data.decode('utf-8'))
+    return to_return
+
+# send_invoice("https://www.cca.edu/sites/default/files/pdf/08/word-to-pdf.pdf")
+# positives = get_positives()
+# negatives = get_negatives()
+# print(positives)
+# print(negatives)

b4940d86e5d5be1d8482043b947c5baaac958d35
Nguyen Hoang Long <nguyeho7@fel.cvut.cz>
2017-06-30 21:00:07 +0200

    main_script

--- main_script.py
+++ main_script.py
@@ -1,8 +1,10 @@
 import convertor
 import get_image_address
 import invoice_crawl
-
+counter = 0
 for link in invoice_crawl.bing_search("invoice", "image"):
+    print(counter)
     url = get_image_address.get_image_url(link['contentUrl'])
     convertor.url_to_pdf(url)
+    counter += 1
 

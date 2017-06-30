91a6221a6b58229653d3a9a28028a430f4fdd89c
Nguyen Hoang Long <nguyeho7@fel.cvut.cz>
2017-06-30 21:30:47 +0200

    occupations final

--- main_script.py
+++ main_script.py
@@ -5,16 +5,17 @@ import occupations
 import os
 import send_invoice
 from time import sleep
-occ = load_occupations()
+occ = occupations.load_occupations()
 
 for oc in occ:
     counter = 0
     print(oc)
-    for link in invoice_crawl.bing_search(oc+"invoice", "image"):
+    for link in invoice_crawl.bing_search(oc+"%20invoice", "image"):
         print(counter)
         url = get_image_address.get_image_url(link['contentUrl'])
         filename = convertor.url_to_pdf(url)
         counter += 1
         os.system("mv "+filename + " ~/WWW/"+filename) 
         sleep(0.6)
-        send_invoice.send_invoice("http://www.ms.mff.cuni.cz/~prochas7/"+filename)
+        #send_invoice.send_invoice("http://www.ms.mff.cuni.cz/~prochas7/"+filename)
+        print(filename)
